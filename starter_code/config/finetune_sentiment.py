f744f78a6261909891d61fc5c8d9ff28c4cd039e
Onatzk <onatzeybekkuskonmaz@hotmail.com>
2025-04-05 22:30:14 +0300

    finetune config

---
+++ starter_code/config/finetune_sentiment.py
@@ -0,0 +1,23 @@
+import time
+
+out_dir = 'out-ft-sentiment'
+eval_interval = 5
+eval_iters = 40
+wandb_log = False # feel free to turn on
+wandb_project = 'sentiment-analysis'
+wandb_run_name = 'ft-' + str(time.time())
+
+dataset = 'customer_service'
+init_from = 'gpt2' # this is the GPT-2 model
+
+# only save checkpoints if the validation loss improves
+always_save_checkpoint = True  
+
+# the number of examples per iter:
+batch_size = 8
+gradient_accumulation_steps = 32
+max_iters = 2000
+
+# finetune at constant LR
+learning_rate = 3e-5
+decay_lr = False
